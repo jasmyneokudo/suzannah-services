01a30437cf9f9007241225630765e2e7c1e031fe
Jasmyne Okudo <jasmyneokudo@gmail.com>
2026-03-23 10:17:39 +0100

    chore: update all prices

--- data/servicePrices.ts
+++ data/servicePrices.ts
@@ -1,11 +1,11 @@
 import { ServiceType } from "@/types/ClientRequest";
 
 export const PRICING: Record<ServiceType, number> = {
-  "Nanny Services": 90500,
+  "Nanny Services": 100500,
   "General Help Services": 95500,
-  "Nanny + Help Services": 95500,
-  "Housekeeper Services": 80500,
-  "Home Cook Services": 150500,
-  "Driving Services": 140500,
-  "Elder Caregiving Services": 200500
+  "Nanny + Help Services": 1055500,
+  "Housekeeper Services": 90500,
+  "Home Cook Services": 170500,
+  "Driving Services": 160500,
+  "Elder Caregiving Services": 220500
 };
