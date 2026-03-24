6cb01718ad60b9ae962fecbeaf2ae23e45dada67
Jasmyne Okudo <jasmyneokudo@gmail.com>
2026-03-24 14:46:56 +0100

    chore: correct price mistake

--- data/servicePrices.ts
+++ data/servicePrices.ts
@@ -3,7 +3,7 @@ import { ServiceType } from "@/types/ClientRequest";
 export const PRICING: Record<ServiceType, number> = {
   "Nanny Services": 100500,
   "General Help Services": 95500,
-  "Nanny + Help Services": 1055500,
+  "Nanny + Help Services": 105500,
   "Housekeeper Services": 90500,
   "Home Cook Services": 170500,
   "Driving Services": 160500,
