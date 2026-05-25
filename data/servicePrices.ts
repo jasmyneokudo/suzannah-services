dbe7ea1d286a209045bc4684e2e841192a7fbfd6
Jasmyne Okudo <jasmyneokudo@gmail.com>
2026-05-25 21:56:09 +0100

    update cookking perices

--- data/servicePrices.ts
+++ data/servicePrices.ts
@@ -5,7 +5,8 @@ export const PRICING: Record<ServiceType, number> = {
   "General Help Services": 95500,
   "Nanny + Help Services": 105500,
   "Housekeeper Services": 90500,
-  "Home Cook Services": 170500,
+  "Home Cook Services": 200500,
+  // take back to 170,500 after eid
   "Driving Services": 160500,
   "Elder Caregiving Services": 220500
 };
